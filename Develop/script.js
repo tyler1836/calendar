bff135084a6b5f0b54a977e5dd68c6b8e462d8e4
unknown <tyler.r.roll@gmail.com>
2021-08-04 23:12:26 -0600

    local storage

--- Develop/script.js
+++ Develop/script.js
@@ -8,29 +8,12 @@ var now = moment();
 var dateEl = document.getElementById("currentDay");
 dateEl.innerHTML = now;
 
-var times = [
-    {hour: 9, id:9},
-    {hour: 10, id:10},
-    {hour: 11, id:11},
-    {hour: 12, id:12},
-    {hour: 1, id:1},
-    {hour: 2, id:2},
-    {hour: 3, id:3},
-    {hour: 4, id:4},
-    {hour: 5, id:5}
-]
-// for (let i = 0; i < times.length; i++) {
-//     var x = document.createElement("div")
-//     x.classList = ("row-mb-4")
-//     append(x) 
-    
-// }
 
-var save = function(){
-    
-    localStorage.setItem("text", textEl.value)
-    
-}
 
-saveEl.addEventListener("click", save)
-console.log(times)
+
+
+$(".saveBtn").on("click", function(){
+    var setHour = $(this).parent().attr("id")
+    var description = $(this).siblings(".description").val()
+    localStorage.setItem(setHour, description)
+})
